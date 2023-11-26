ba5d3640a7fc781bd8ce124cb80d26701e85ea4b
MuhammadSaddique <97628071+MuhammadSaddique@users.noreply.github.com>
2023-11-27 00:33:29 +0500

    button added

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
-var button = document.querySelector("button");
-var h1 = document.querySelector("h1");
 
-button.addEventListener("click",function(){
-   
-    var newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
-
-var makeRandColor = () =>{
-    
-    var r = Math.floor(Math.random() *255);
-    var g = Math.floor(Math.random() *255);
-    var b = Math.floor(Math.random() *255);
-    return `rgb(${r},${g},${b})`;
+var makeRandColor = function() {
+    var r = Math.floor(Math.random()*255);
+    var g = Math.floor(Math.random()*255);
+    var b = Math.floor(Math.random()*255);
+    return `rgb(${r},${g},${b})`
 }
+
+
+
+// var buttons = document.querySelectorAll("button");
+// var h1s = document.querySelectorAll("h1");
+
+// for(let button of buttons){
+//     button.addEventListener("click",function(){
+//         button.style.backgroundColor = makeRandColor();
+//         button.style.color = makeRandColor();
+//     })
+// }
+
+
+// for(let h1 of h1s){
+//     h1.addEventListener("click",function(){
+//         h1.style.backgroundColor = makeRandColor();
+//         h1.style.color = makeRandColor();
+        
+//     })
+// }
+
+
+///////////////////////// or
+
+
+var buttons = document.querySelectorAll("button");
+for(let button of buttons){
+    button.addEventListener("click",colorize)
+}
+
+
+var h1s = document.querySelectorAll("h1");
+
+for(let h1 of h1s){
+    h1.addEventListener("click", colorize)
+}
+
+
+
+function colorize(){
+    this.style.backgroundColor = makeRandColor()
+    this.style.color = makeRandColor()
+}
+
+
+
+
 
